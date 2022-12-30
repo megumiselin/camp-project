4e02cddac0bb9d098e9ef680936025af9d44662f
megumiselin <119290046+megumiselin@users.noreply.github.com>
2022-12-30 15:21:39 +0300

    Add files via upload
    Component Oluşturma

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+       <h1>Hello React</h1>
+       <Header/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,11 @@
+
+
+
+function Header () {
+    return(
+        <div>
+             Merhaba ben Header bileşeni
+        </div>
+    )
+}
+export default Header;
