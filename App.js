d3b144424daced1141569ff711e28e201d2a7839
megumiselin <119290046+megumiselin@users.noreply.github.com>
2023-01-02 09:15:32 +0300

    Add files via upload
    Rendering Elements

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './App.css';
-import Header from './components/Header'
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
-  return (
-    <div>
-       <h1>Hello React</h1>
-       <Header/>
-        
-    </div>
-  );
-}
+  
+  function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    root.render(element);
 
-export default App;
+  }
+  setInterval(tick, 1000);
+}
+export default App ;
+
+
+
